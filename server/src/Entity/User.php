796f811151785477f52b3999ff8d2677678ecac6
Ndifreke Ekott <ndy40.ekott@gmail.com>
2020-06-27 18:18:29 +0100

    More files i forgot to add

---
+++ server/src/Entity/User.php
@@ -0,0 +1,187 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+use App\Http\Requests\SignUpRequests;
+
+/**
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @ORM\Table(name="users")
+ * @ApiResource(
+ *     shortName="users",
+ *     normalizationContext={"groups"={"user:read"}},
+ *     itemOperations={"get"},
+ *     collectionOperations={
+ *      "register"={
+ *          "method"="post",
+ *          "path"="/users/register",
+ *          "input"=SignUpRequests::class,
+ *       },
+ *       "reset_password"={
+ *          "method": "POST",
+ *          "path"="/users/reset_password",
+ *          "denormalization_context"={"groups"={"user:reset_password"}}
+ *       },
+ *       "change_password"={
+ *          "method"="POST",
+ *          "path"="/users/change_password",
+ *          "denormalization_context"={"groups"={"user:change_password"}}
+ *       }
+ *     }
+ * )
+ */
+class User implements UserInterface
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     * @Groups({"user:read"})
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=180, unique=true)
+     * @Assert\Email()
+     * @Groups({"user:register", "user:reset_password", "user:read"})
+     * @Assert\NotNull(groups={"user:register", "user:reset_password"})
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
+
+    /**
+     * @var string The hashed password
+     * @ORM\Column(type="string")
+     * @Groups({"user:register", "user:reset_password", "user:change_password"})
+     * @Assert\NotNull(groups={"user:register", "user:reset_password"})
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Groups({"user:register", "user:read"})
+     * @Assert\Type(type="string")
+     * @Assert\NotNull(groups={"user:register"})
+     */
+    private $firstName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\Type(type="string")
+     * @Groups({"user:register", "user:read"})
+     * @Assert\NotNull(groups={"user:register"})
+     */
+    private $lastName;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getPassword(): string
+    {
+        return (string) $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getSalt()
+    {
+        // not needed when using the "bcrypt" algorithm in security.yaml
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+}
